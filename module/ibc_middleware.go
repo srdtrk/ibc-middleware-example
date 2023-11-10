97ec229ba16f8029df2904096c70c5cc8abeecce
srdtrk <srdtrk@hotmail.com>
2023-11-11 00:49:28 +0300

    feat: created 'ibc_middleware.go' file

---
+++ module/ibc_middleware.go
@@ -0,0 +1,38 @@
+package module
+
+import (
+	"errors"
+
+	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
+	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
+
+	"github.com/cosmosregistry/example/keeper"
+)
+
+// IBCMiddleware implements the ICS26 callbacks for linked-packets given the
+// linked-packets keeper and the underlying application.
+type IBCMiddleware struct {
+	app         porttypes.IBCModule
+	ics4Wrapper porttypes.ICS4Wrapper
+
+	keeper keeper.Keeper
+}
+
+// NewIBCMiddleware creates a new IBCMiddlware given the keeper and underlying application
+func NewIBCMiddleware(app porttypes.IBCModule, ics4Wrapper porttypes.ICS4Wrapper, k keeper.Keeper) IBCMiddleware {
+	if app == nil {
+		panic(errors.New("IBCModule cannot be nil"))
+	}
+
+	if ics4Wrapper == nil {
+		panic(errors.New("ICS4Wrapper cannot be nil"))
+	}
+
+	return IBCMiddleware{
+		app:         app,
+		ics4Wrapper: ics4Wrapper,
+		keeper:      k,
+	}
+}
+
+
