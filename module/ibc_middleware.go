da2bf41dd9d432d632f585271236e63de9fd7f2f
srdtrk <srdtrk@hotmail.com>
2023-11-15 13:02:30 +0300

    imp: added some functions to implement the middleware interface (wip)

--- module/ibc_middleware.go
+++ module/ibc_middleware.go
@@ -3,11 +3,16 @@ package module
 import (
 	"errors"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
 	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
+	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
 
 	"github.com/cosmosregistry/example/keeper"
 )
+
+var _ porttypes.Middleware = (*IBCMiddleware)(nil)
 
 // IBCMiddleware implements the ICS26 callbacks for linked-packets given the
 // linked-packets keeper and the underlying application.
@@ -35,4 +40,17 @@ func NewIBCMiddleware(app porttypes.IBCModule, ics4Wrapper porttypes.ICS4Wrapper
 	}
 }
 
+// WriteAcknowledgement implements the ICS4 Wrapper interface
+func (im IBCMiddleware) WriteAcknowledgement(
+	ctx sdk.Context,
+	chanCap *capabilitytypes.Capability,
+	packet ibcexported.PacketI,
+	ack ibcexported.Acknowledgement,
+) error {
+	return im.ics4Wrapper.WriteAcknowledgement(ctx, chanCap, packet, ack)
+}
 
+// GetAppVersion returns the application version of the underlying application
+func (im IBCMiddleware) GetAppVersion(ctx sdk.Context, portID, channelID string) (string, bool) {
+	return im.ics4Wrapper.GetAppVersion(ctx, portID, channelID)
+}
